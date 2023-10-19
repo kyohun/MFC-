7fb54f9cf5b1b4723ceae8d5abefe7fcb602504c
kyohun <105188216+kyohun@users.noreply.github.com>
2023-10-19 12:31:30 +0900

    Add files via upload

---
+++ CU완/common/ordef.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define REG_PNAME	TEXT("\\\\.\\pipe\\Reg")
+#define MAX_NAME_LEN	256
+#define MAX_ID_LEN		100
+#define MAX_PW_LEN		20
+#define MAX_PRICE_LEN	20
+#define MAX_TOTAL_LEN	20
+
+#define ORD_RES_OK		0
+#define ORD_RES_NO		-1
+
+#define ORD_REQ		0x0004
+#define ORD_RES		0x0005
+#define UNORD_REQ	0x0006
